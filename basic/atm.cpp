0a878f528ba5428e0b1086f7dfa5d2de68679ba8
Santhoji07 <santhoji07msd@gmail.com>
2025-07-22 16:05:37 +0530

    basic

---
+++ basic/atm.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class ATM
+{
+    int pin;
+    int balance;
+public:
+    ATM()
+    {
+        pin = 1234;
+        balance = 420;
+    }
+
+    void print_balance() {
+        cout << balance << endl;
+    }
+
+    void set_pin(int oldA_pin, int new_pin)
+    {
+        if (old_pin == pin)
+        {
+            pin = new_pin;
+            cout << "Updated successfully\n";
+        }
+        else cout << "Invalid old pin";
+    }
+};
+
+int main() {
+    ATM A1;
+    A1.print_balance();
+    A1.set_pin(1234, 5673);
+    A1.set_pin(1234,2770);
+}
