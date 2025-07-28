6976411e1e86b93fb4c0b4932bced8b232ad5c0f
Santhoji07 <santhoji07msd@gmail.com>
2025-07-28 10:53:36 +0530

    basic

---
+++ basic/secondlargest.java
@@ -0,0 +1,24 @@
+public class aa {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 9, 6, 3, 9}; // 
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("There is no second largest element.");
+        } else {
+            System.out.println("Second largest number is: " +secondLargest);
+        }
+    }
+}
