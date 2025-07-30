5176b27c7c71651e835fb7633fb622946f5312de
Santhoji07 <santhoji07msd@gmail.com>
2025-07-30 09:38:39 +0530

    basic

---
+++ basic/bubblesort.java
@@ -0,0 +1,33 @@
+public class bubblesort {
+
+    public static int BubbleSorting(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        if (n != 0) {
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                    if (nums[j] > nums[j + 1]) {
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String args[]) {
+        int nums[] = {23, 45, 24, 66};
+        int swaps = BubbleSorting(nums);
+
+        System.out.println("Number of swaps: " + swaps);
+        System.out.print("Sorted array: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
